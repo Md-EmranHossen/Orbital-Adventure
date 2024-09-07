2cd1888f302ea0b2609fcadfbfadaba898da94d5
Md. Emran Hossen <emranhossen3075@gmail.com>
2024-09-07 18:53:20 +0600

    add files

---
+++ Assets/Script/ItemCollector.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ItemCollector : MonoBehaviour
+{
+    private int cherries = 0;
+
+    [SerializeField] private TextMeshProUGUI cherriesText;
+    [SerializeField] private AudioSource collectionSoundEffect;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Cherry"))
+        {
+            collectionSoundEffect.Play();
+            Destroy(collision.gameObject);
+            cherries++;
+            cherriesText.text = "Cherries: " + cherries;
+        }
+    }
+}
